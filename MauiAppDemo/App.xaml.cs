081b4306bc68134d3dac5a62c84126dc17bd4ce8
Diogo Sanlez <diogosdl25@gmail.com>
2024-09-30 16:43:20 +0100

    Stacklayout Page

--- MauiAppDemo/App.xaml.cs
+++ MauiAppDemo/App.xaml.cs
@@ -6,12 +6,7 @@
         {
             InitializeComponent();
 
-            var navPage = new NavigationPage(new MinhaPagina());
-
-            navPage.BarBackground = Colors.Yellow;
-            navPage.BarTextColor = Colors.Blue;
-
-            MainPage = new TabbedPageDemo();
+            MainPage = new NavigationPage(new StackLayoutDemo());
         }
     }
 }
---
+++ MauiAppDemo/StackLayoutDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppDemo;
+
+public partial class StackLayoutDemo : ContentPage
+{
+	public StackLayoutDemo()
+	{
+		InitializeComponent();
+	}
+}
