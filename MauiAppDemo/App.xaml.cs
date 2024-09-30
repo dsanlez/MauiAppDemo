49f12d532bb985fea3ab8b5a99eb327dc61c3c34
Diogo Sanlez <diogosdl25@gmail.com>
2024-09-30 14:19:04 +0100

    MyPage

--- MauiAppDemo/App.xaml.cs
+++ MauiAppDemo/App.xaml.cs
@@ -6,7 +6,12 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            var navPage = new NavigationPage(new MinhaPagina());
+
+            navPage.BarBackground = Colors.Yellow;
+            navPage.BarTextColor = Colors.Blue;
+
+            MainPage = navPage;
         }
     }
 }
---
+++ MauiAppDemo/MinhaPagina.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppDemo;
+
+public partial class MinhaPagina : ContentPage
+{
+	public MinhaPagina()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+		await Navigation.PushAsync(new MainPage());
+    }
+}
