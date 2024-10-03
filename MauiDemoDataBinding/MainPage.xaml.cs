345f30d4d8117867c2275fb26bb6914a571d82df
Diogo Sanlez <diogosdl25@gmail.com>
2024-10-03 11:30:16 +0100

    DataBinding 1

---
+++ MauiDemoDataBinding/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using MauiDemoDataBinding.Models;
+
+namespace MauiDemoDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void CounterBtn_Clicked(object sender, EventArgs e)
+        //{
+        //    var product = new Product
+        //    {
+        //        ProductName = "iPhone 5",
+        //        Price = 1000.00m,
+        //        Stock = 5
+        //    };
+        //    //Binding productBinding = new Binding();
+        //    //productBinding.Source = product;
+        //    //productBinding.Path = "ProductName";
+
+        //    //lblName.SetBinding(Label.TextProperty, productBinding);
+
+        //    Binding nameBinding = new Binding
+        //    {
+        //        Source = product,
+        //        Path = "ProductName",
+        //        StringFormat = "Product: {0}"
+        //    };
+        //    lblName.SetBinding(Label.TextProperty, nameBinding);
+        //    Binding priceBinding = new Binding
+        //    {
+        //        Source = product,
+        //        Path = "Price",
+        //        StringFormat = "Price: {0}"
+        //    };
+        //    lblPrice.SetBinding(Label.TextProperty, priceBinding);
+        //    Binding stockBinding = new Binding
+        //    {
+        //        Source = product,
+        //        Path = "Stock",
+        //        StringFormat = "Stock: {0}"
+        //    };
+        //    lblStock.SetBinding(Label.TextProperty, stockBinding);
+        //}
+    }
+    
+}
